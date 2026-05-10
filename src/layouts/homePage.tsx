e5243e7d4e8017a55349835212939a715bae3c8d
Mateusz Ogonek <ogonekmateusz69@gmail.com>
2026-05-10 12:12:55 +0200

    addd HowItWorks section with heading

--- src/layouts/homePage.tsx
+++ src/layouts/homePage.tsx
@@ -2,16 +2,20 @@ import Navbar from "../components/homePage/Navbar";
 import Hero from "../sections/homePage/Hero";
 import Stats from "../sections/homePage/Stats";
 import CarsLibrary from "../sections/homePage/CarsLibrary";
+import HowItWorks from "../sections/homePage/HowItWorks";
 
 export default function homePage() {
   return (
     <div>
       <Navbar />
-      <div className="bg-gray-50">
+      <section className="bg-gray-50">
         <Hero />
         <Stats />
         <CarsLibrary />
-      </div>
+      </section>
+      <section className="bg-gray-100">
+        <HowItWorks />
+      </section>
     </div>
   );
 }
---
+++ src/sections/homePage/HowItWorks.tsx
@@ -0,0 +1,13 @@
+import SectionContainer from "../../components/shared/SectionContainer";
+
+export default function HowItWorks() {
+  return (
+    <div className="py-15 md:py-20 lg:py-25" id="offer">
+      <SectionContainer>
+        <h3 className="text-4xl md:text-5xl font-medium md:font-semibold text-center">
+          Prosta Droga do Luksusu
+        </h3>
+      </SectionContainer>
+    </div>
+  );
+}
