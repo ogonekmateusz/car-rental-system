df590346896500f3703444204e63cd290e6aec5c
Mateusz Ogonek <ogonekmateusz69@gmail.com>
2026-05-11 21:41:08 +0200

    refactor Footer component for improved layout and styling

--- src/sections/homePage/Footer.tsx
+++ src/sections/homePage/Footer.tsx
@@ -6,8 +6,9 @@ export default function Footer() {
   return (
     <footer className="py-8 bg-black text-white">
       <SectionContainer>
-        <RowBetween>
-          <Logo size="text-2xl lg:text-3xl" />
+        <RowBetween className="flex-col items-start gap-3 md:flex-row md:items-center md:gap-0">
+          <Logo size="text-xl lg:text-3xl" />
+
           <p className="text-sm text-gray-400">
             © 2026 Drive. Wszystkie prawa zastrzeżone.
           </p>
