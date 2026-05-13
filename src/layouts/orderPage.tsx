cd8bf98d0525a1ffbbc8560e6ed8e9b18621cb47
Mateusz Ogonek <ogonekmateusz69@gmail.com>
2026-05-13 11:34:15 +0200

    add order form Component with title

--- src/layouts/orderPage.tsx
+++ src/layouts/orderPage.tsx
@@ -1,12 +1,14 @@
 import Navbar from "../components/homePage/Navbar.tsx";
-
+import OrderForm from "../components/orderPage/orderForm.tsx";
+import Footer from "../sections/homePage/Footer.tsx";
 export default function orderPage() {
     return (
         <div>
             <Navbar />
             <section className="bg-gray-50">
-                lorremvdsgcfsdrgh
+                <OrderForm/>
             </section>
+            <Footer/>
         </div>
     )
 }
