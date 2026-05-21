cabaab275a507fd4b0dc144f860f0cb36ab6a863
Mateusz Ogonek <ogonekmateusz69@gmail.com>
2026-05-21 17:49:15 +0200

    add reusable global admin panel section component

---
+++ src/components/shared/AdminPanelSection.tsx
@@ -0,0 +1,60 @@
+import SectionContainer from "./SectionContainer.tsx";
+import type { ReactNode } from "react";
+import PrimaryButton from "./PrimaryButton.tsx";
+
+type AdminPanelSectionProps = {
+  children: ReactNode;
+  header: string;
+  topText?: string;
+  sectionHeadingButtonTitle?: string;
+  sectionHeadingOnClick?: () => void;
+};
+
+export default function AdminPanelSection({
+  children,
+  header,
+  topText,
+  sectionHeadingButtonTitle,
+  sectionHeadingOnClick,
+}: AdminPanelSectionProps) {
+  return (
+    <section className="w-full py-6">
+      <SectionContainer>
+        <header className="mb-8 flex flex-col gap-5 md:flex-row md:justify-between md:gap-8">
+          <div>
+            {topText && (
+              <span className="mb-2 block text-[11px] font-bold uppercase tracking-widest text-blue-600">
+                {topText}
+              </span>
+            )}
+
+            <h2 className="text-4xl font-bold leading-none tracking-tight">
+              {header}
+            </h2>
+          </div>
+
+          {sectionHeadingButtonTitle && sectionHeadingOnClick && (
+            <PrimaryButton
+              onClick={sectionHeadingOnClick}
+              className="
+                h-fit
+                w-full
+                md:w-auto
+                px-6
+                py-3
+                text-xs
+                font-bold
+                uppercase
+                tracking-wide
+              "
+            >
+              {sectionHeadingButtonTitle}
+            </PrimaryButton>
+          )}
+        </header>
+
+        {children}
+      </SectionContainer>
+    </section>
+  );
+}
---
+++ src/sections/adminPage/Cars.tsx
@@ -0,0 +1,14 @@
+import AdminPanelSection from "../../components/shared/AdminPanelSection.tsx";
+
+export default function Cars() {
+  return (
+    <AdminPanelSection
+      header={"Zarządzanie Flotą"}
+      sectionHeadingButtonTitle={"+ DODAJ NOWY SAMOCHÓD"}
+      sectionHeadingOnClick={() => {}}
+      topText={"system zarządzania"}
+    >
+      sdg
+    </AdminPanelSection>
+  );
+}
